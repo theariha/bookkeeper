77fe5d11ceae01af9e76ded38cc38ef6be339d2b
theariha <73840738+theariha@users.noreply.github.com>
2024-03-29 22:59:06 +0300

    добавлены тесты

---
+++ tests/test_repository/test_sqlite_repository.py
@@ -0,0 +1,76 @@
+from bookkeeper.repository.sqlite_repository import SQliteRepository
+import pytest
+
+from dataclasses import dataclass
+
+
+@pytest.fixture
+def custom_class():
+    @dataclass
+    class Custom:
+        pk: int = 0
+        expen: float = 0.0
+        shop: str = ""
+        it: int = 0
+
+    return Custom
+
+
+@pytest.fixture
+def repo(tmp_path, custom_class):
+    return SQliteRepository(tmp_path / "test_data.db", custom_class)
+
+
+def test_crud(repo, custom_class):
+    obj = custom_class()
+    pk = repo.add(obj)
+    assert obj.pk == pk
+    assert repo.get(pk) == obj
+    obj2 = custom_class()
+    obj2.pk = pk
+    repo.update(obj2)
+    assert repo.get(pk) == obj2
+    repo.delete(pk)
+    assert repo.get(pk) is None
+
+
+def test_cannot_add_with_pk(repo, custom_class):
+    obj = custom_class()
+    obj.pk = 1
+    with pytest.raises(ValueError):
+        repo.add(obj)
+
+
+def test_cannot_add_without_pk(repo):
+    with pytest.raises(ValueError):
+        repo.add(0)
+
+
+def test_cannot_delete_unexistent(repo):
+    with pytest.raises(KeyError):
+        repo.delete(1)
+
+
+def test_cannot_update_without_pk(repo, custom_class):
+    obj = custom_class()
+    with pytest.raises(ValueError):
+        repo.update(obj)
+
+
+def test_get_all(repo, custom_class):
+    objects = [custom_class() for i in range(5)]
+    for o in objects:
+        repo.add(o)
+    assert repo.get_all() == objects
+
+
+def test_get_all_with_condition(repo, custom_class):
+    objects = []
+    for i in range(43):
+        o = custom_class()
+        o.it = i
+        o.shop = "test"
+        repo.add(o)
+        objects.append(o)
+    assert repo.get_all({"shop": "test"}) == objects
+    assert repo.get_all({"it": 42}) == [objects[-1]]
