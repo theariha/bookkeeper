86289f86212c7a5283210251b7153ce85a100702
theariha <73840738+theariha@users.noreply.github.com>
2024-04-07 23:49:11 +0300

    создана модель бюджета

---
+++ bookkeeper/models/budget.py
@@ -0,0 +1,15 @@
+"""
+Модель бюджета
+"""
+import datetime
+from dataclasses import dataclass
+
+@dataclass
+class Budget:
+    """
+    Бюджет содержит срок (term), сумму (summa) и категорию расходов (category)
+    """
+    summa: float = 0
+    term: int = 0
+    category: str = ""
+    pk: int = 0
